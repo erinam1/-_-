058d51d44e450b9d4a228424485284d855c85ae6
erina_m <erina.marina.alex@gmail.com>
2015-11-25 21:22:33 +0300

    Модель. Первая иттерация, почти вся

---
+++ Зарплата_ФИО/Работник.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зарплата_ФИО
+{
+    public class Работник
+    {
+        readonly int рабНом;
+        string фио;
+        string адрес;
+        public Работник(int рабНом, string фио, string адрес)
+        {
+            this.рабНом = рабНом;
+            this.фио = фио;
+            this.адрес = адрес;
+        }
+        public string ФИО
+        {
+            get { return фио; }
+            set { фио = value; }
+        }
+        public string Address
+        {
+            get { return адрес; }
+            set { адрес = value; }
+        }
+        public int РабНом
+        {
+            get { return рабНом; }
+        }
+    }
+}
